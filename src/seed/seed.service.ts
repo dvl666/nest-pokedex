89572bab3e18edbb82f12acd4f1332588346eb29
dvl666 <kamagaspar@gmail.com>
2024-11-19 16:58:14 -0300

    update seed. service

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,26 +1,33 @@
 import { HttpModule, HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { CreatePokemonDto } from 'src/pokemon/dto/create-pokemon.dto';
+import { PokemonService } from 'src/pokemon/pokemon.service';
 
 @Injectable()
 export class SeedService {
 
     constructor(
-        private readonly http: HttpService
+        private readonly http: HttpService,
+        private readonly pokemonService: PokemonService
     ) {}
 
 
     async executeSeed() {
+        let newPokemons: CreatePokemonDto[] = [];
         const { data } = await this.http.axiosRef.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=20');
-        console.log('Seed executed');
-
+        
         data.results.forEach(({ name, url }) => {
+            name = name.toLowerCase();
             const segments = url.split('/');
             const no:number = +segments[segments.length - 2];
-            console.log({ name, no });
+            newPokemons.push({ name, no });
         })
-
-        return data.results;
+        
+        newPokemons = await this.pokemonService.createMany(newPokemons);
+        console.log('Seed executed');
+        return newPokemons;
     }
 
 }
